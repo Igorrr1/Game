72c88ab5048b37ba1148ba61ba06da6690b12d7f
User Name <dyakovgarick@yandex.ru>
2021-07-12 16:50:09 +0300

    first commit

---
+++ src/domain/monsters/lvl1/Phoenix.java
@@ -0,0 +1,9 @@
+package domain.monsters.lvl1;
+
+import entity.FantasyCharacter;
+
+public class Phoenix extends FantasyCharacter {
+    public Phoenix(String name, int healthPoints, int strength, int dexterity, int xp, int gold) {
+        super(name, healthPoints, strength, dexterity, xp, gold);
+    }
+}
---
+++ src/domain/monsters/lvl1/Tauren.java
@@ -0,0 +1,9 @@
+package domain.monsters.lvl1;
+
+import entity.FantasyCharacter;
+
+public class Tauren extends FantasyCharacter {
+    public Tauren(String name, int healthPoints, int strength, int dexterity, int xp, int gold) {
+        super(name, healthPoints, strength, dexterity, xp, gold);
+    }
+}
